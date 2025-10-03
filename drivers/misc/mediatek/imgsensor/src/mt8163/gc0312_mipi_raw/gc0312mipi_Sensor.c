36ba133f1a36f47ebf9491eb0a2d93943bf432e6
bengris32 <bengris32@protonmail.ch>
2025-10-03 21:36:03 +0100

    imgsensor: gc0312_mipi_raw: Unbreak get_default_framerate_by_scenario()
    Change-Id: I0b565f2782a87a93de69e9e60991c217e36eb942
    Signed-off-by: bengris32 <bengris32@protonmail.ch>

--- drivers/misc/mediatek/imgsensor/src/mt8163/gc0312_mipi_raw/gc0312mipi_Sensor.c
+++ drivers/misc/mediatek/imgsensor/src/mt8163/gc0312_mipi_raw/gc0312mipi_Sensor.c
@@ -1169,6 +1169,12 @@ static kal_uint32 get_default_framerate_by_scenario(enum MSDK_SCENARIO_ID_ENUM s
 {
     LOG_INF("scenario_id = %d\n", scenario_id);
 
+    if (framerate == NULL)
+    {
+        LOG_INF("---- framerate is NULL ---- check here \n");
+        return ERROR_NONE;
+    }
+
     switch (scenario_id) {
         case MSDK_SCENARIO_ID_CAMERA_PREVIEW:
             *framerate = imgsensor_info.pre.max_framerate;
@@ -1256,7 +1262,7 @@ static kal_uint32 feature_control(MSDK_SENSOR_FEATURE_ENUM feature_id,
             set_max_framerate_by_scenario((enum MSDK_SCENARIO_ID_ENUM)*feature_data, *(feature_data+1));
             break;
         case SENSOR_FEATURE_GET_DEFAULT_FRAME_RATE_BY_SCENARIO:
-            get_default_framerate_by_scenario((enum MSDK_SCENARIO_ID_ENUM)*(feature_data), (MUINT32 *)(uintptr_t)(*(feature_data+1)));
+		    get_default_framerate_by_scenario((enum MSDK_SCENARIO_ID_ENUM)*(feature_data), (MUINT32 *)(uintptr_t)(feature_data+1));
             break;
         case SENSOR_FEATURE_SET_TEST_PATTERN:
             set_test_pattern_mode((BOOL)*feature_data);
